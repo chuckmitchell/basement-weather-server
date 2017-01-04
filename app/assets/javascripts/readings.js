c7d840e4c9d003fa0a4e16cf927476319e16cec4
Charles Mitchell <cmitchell@kanayo.com>
2017-01-04 14:24:05 -0400

    Display xAxis dates in format Jan 18 - 1am

--- app/assets/javascripts/readings.js
+++ app/assets/javascripts/readings.js
@@ -132,7 +132,7 @@ var loadChart = function(container, dataPoints, options) {
   var chart = new CanvasJS.Chart(container, {
       zoomEnabled: true, 
       title: title,
-      axisX: { valueFormatString: "MMM DD hh TT", labelAngle: -20 },
+      axisX: { valueFormatString: "MMM D - htt", labelAngle: -20 },
       axisY: axisY,
       toolTip: toolTip,
       data: data
